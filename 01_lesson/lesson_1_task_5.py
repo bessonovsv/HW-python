ec912152a8e984027f866545bad9e7ef6f4451e4
bessonovsv <bessonovsv1975@gmail.com>
2025-02-24 12:42:33 +0300

    HW-1

---
+++ 01_lesson/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_num():
+    print(88005553535)
+
+
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
+print_num()
